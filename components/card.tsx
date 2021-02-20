b095d0cdb2d7d9d0982d20b3f9465fc20ac4fa34
ktny <katsunori.t10@gmail.com>
2021-02-20 21:13:21 +0900

    add flip function

--- components/card.tsx
+++ components/card.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 import styles from "../styles/card.module.scss";
 
 export default function Card() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={`${styles.card}`}>
+    <div
+      className={`${styles.card} ${open ? styles.open : ""}`}
+      onClick={() => setOpen(!open)}
+    >
       <div className={styles.front}>quote</div>
       <div className={styles.back}>見積もり</div>
     </div>
