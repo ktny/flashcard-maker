7de624a311a585cc88a875f5900437266ce87f2c
ktny <katsunori.t10@gmail.com>
2021-02-23 00:13:10 +0900

    import gss cards

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import styles from "../styles/header.module.scss";
@@ -5,9 +6,11 @@ import styles from "../styles/header.module.scss";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div>
-        <Image priority src="/icon.png" height={44} width={44} />
-      </div>
+      <Link href="/" passHref>
+        <a>
+          <Image priority src="/icon.png" height={44} width={44} />
+        </a>
+      </Link>
     </header>
   );
 }
